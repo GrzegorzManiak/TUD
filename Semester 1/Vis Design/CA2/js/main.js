a8dadcc491ade4c84b1d301aa6a032edec32da58
KetamineKyle <greg@ap3x.xyz>
2021-12-08 01:21:22 +0000

    Comments

--- Semester 1/Vis Design/CA2/js/main.js
+++ Semester 1/Vis Design/CA2/js/main.js
@@ -3,7 +3,8 @@ const pages = {
     'ui/ux': './uxui.html'
 }
 
-let currentPageName,
+let stack = [],
+    currentPageName,
     scroll,
     height;
 
@@ -17,15 +18,33 @@ function sleep(ms) {
     })
 }
 
-function currentPage(page) {
-    currentPageName = page;
+function hamburger(id) {
+    let hamburgerBtn = document.getElementById(id),
+        navLogo = document.getElementById('navLogo'),
+        hamburgerContent = document.getElementById('hamburgerContent');
+
+    hamburgerBtn.addEventListener('click', () => {
+        switch (hamburgerBtn.checked) {
+            case true:
+                hamburgerContent.className = 'hamburgerContentVisible';
+                document.documentElement.className = 'htmlStopScroll';
+                navLogo.className = 'logoColorMenuOpen';
+                break;
+
+            case false:
+                hamburgerContent.className = 'hamburgerContentInvisible';
+                document.documentElement.className = '';
+                navLogo.className = 'logoColorMenuClosed';
+                break;
+        }
+    });
 }
 
 // adds elements to the nav bar from the 'pages' object.
 function addElements(navID, selected) {
     // Get the navbar element
     let element = document.getElementById(navID);
-    
+
     // For each item in the Pages object
     Object.keys(pages).forEach((key) => {
         // create a bew LI element and a new P element
@@ -33,7 +52,7 @@ function addElements(navID, selected) {
             p = document.createElement('p');
 
         // Assign the LI an ID
-        li.id = key;
+        li.class = key;
 
         // Assign the P its text content
         p.textContent = key;
@@ -55,7 +74,10 @@ function addElements(navID, selected) {
 }
 
 function selectPageIndicator(num) {
+    // get the elements inside the 'pageIndicator' element
     let elements = [...document.getElementsByClassName('pageIndicatorElement')];
+    // convert it from a Dom collection to an array
+
     elements.forEach((elm, i) => {
         if (i == num && i < elements.length) elm.className = 'pageIndicatorElement pageIndicatorElementActive';
         else elm.className = 'pageIndicatorElement';
@@ -71,7 +93,7 @@ async function startScroll() {
 
     while (true) {
         // Get the current scroll position  
-        scroll = document.documentElement.scrollTop; // Scroll top dosent work on mobile?
+        scroll = document.scrollingElement.scrollTop; // Scroll top dosent work on mobile?
         height = document.documentElement.clientHeight;
 
         await sleep(5);
@@ -94,7 +116,7 @@ async function startScroll() {
         currentPage = Math.round(scroll / height);
         selectPageIndicator(currentPage);
 
-        if(currentPage == 1 && !loaded) {
+        if (currentPage == 1 && !loaded) {
             await sleep(100);
             addCarouselItems();
             loaded = true;
@@ -105,33 +127,29 @@ async function startScroll() {
 }
 
 const imgs = {
-    '0': { 
-        //The path to the image
+    '0': {
         img: './imgs/aboutus/1.jpg',
-        //Onece the image is loaded, this will be set to the image element
         elem: undefined,
-        //The name of the group memeber
-        name:'Thomas',
-        //Their bio
+        name: 'Greg',
         bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, laboris nisi ut aliquip ex ea commodo consequat.'
     },
-    '1': { 
-        img: './imgs/aboutus/2.jpg', 
+    '1': {
+        img: './imgs/aboutus/2.jpg',
         elem: undefined,
-        name:'Grzegorz',
-        bio: 'Hey, my name is Grzegorz Maniak, I\'m studying computing at TU Tallaght campus, I have a great interest in web and software development, I also enjoy the outdoors, I love to travel, camp and hike.' 
+        name: 'Grzegorz',
+        bio: 'Hey, my name is Grzegorz Maniak, I\'m studying computing at TU Tallaght campus, I have a great interest in web and software development, I also enjoy the outdoors, I love to travel, camp and hike.'
     },
-    '2': { 
-        img: './imgs/aboutus/3.jpg', 
+    '2': {
+        img: './imgs/aboutus/3.jpg',
         elem: undefined,
-        name:'Moyin',
-        bio: 'My name is Moyin Olusona. I am 17 years old and am in my first year of college. I went to St. Louis High school. My favourite subjects were English and Geography. I like to read and watch tv and movies.' 
+        name: 'Gregor',
+        bio: 'My name is Moyin Olusona. I am 17 years old and am in my first year of college. I went to St. Louis High school. My favourite subjects were English and Geography. I like to read and watch tv and movies.'
     },
-    '3': { 
-        img: './imgs/aboutus/4.jpg', 
+    '3': {
+        img: './imgs/aboutus/4.jpg',
         elem: undefined,
-        name:'Jan',
-        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, laboris nisi ut aliquip ex ea commodo consequat.' 
+        name: 'Gorg',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, laboris nisi ut aliquip ex ea commodo consequat.'
     },
 }
 
@@ -146,38 +164,40 @@ function addCarouselItems() {
 
         //add text to the div
         div.innerHTML = `<h2 class='carouselText'>${imgs[key].name}</h2><img class='carouselImg' src="${imgs[key].img}" alt="${imgs[key].name}"><p class='carouselText'>${imgs[key].bio}</p>`;
-        
+
         //give the div a name
         div.name = key;
 
-        if (i == 0) {
-            div.className = 'scaleOutToLeft';
-            div.id = 'carouselLeft';
-        } else if (i == 1) {
-            div.className = 'carouselSelected';
-            div.id = 'carouselCenter';
-            div.getElementsByTagName('img')[0].useMap = '#aboutus';
-        } else if (i == 2) {
-            div.className = 'scaleOutToRight';
-            div.id = 'carouselRight';
-        } else {
-            stack = [div, ...stack];
-            div.className = 'invisible';
+        switch (i) {
+            case 0: // Image on the left hand side
+                div.className = 'scaleOutToLeft';
+                div.id = 'carouselLeft';
+                break;
+
+            case 1: // Image in the middle
+                div.className = 'carouselSelected';
+                div.id = 'carouselCenter';
+                div.getElementsByTagName('img')[0].useMap = '#aboutus';
+                break;
+
+            case 2: // Image on the right hand side
+                div.className = 'scaleOutToRight';
+                div.id = 'carouselRight';
+                break;
+
+            default:
+                // Since this image wont be seen, add it to the stack
+                stack = [div, ...stack];
+                // and make it invisable
+                div.className = 'invisible';
+                break;
         }
 
         imgs[key].elem = div;
         carousel.appendChild(div);
-    });
 
-    addCarouselInteractions();
-}
-
-let stack = [];
-
-function addCarouselInteractions() {
-    Object.keys(imgs).forEach((key) => {
-        imgs[key].elem.addEventListener('click', (elm) => {
-            carouselScroll(imgs[key].elem);
+        div.addEventListener('click', () => {
+            carouselScroll(div);
         });
     });
 }
@@ -234,7 +254,7 @@ function carouselScroll(elem) {
             cs.id = 'carouselRight'; //lable the imgs position
             cs.className = 'scaleOutToRight'; //animate the move
 
-            break;
+            return;
 
         case 'Right': // move the img to the left
             //If theres an image to the left, move it behind the center
@@ -247,40 +267,22 @@ function carouselScroll(elem) {
             if (!document.getElementById('carouselRight')) {
                 stack[stack.length - 1].className = 'moveRightFromCenter'; //animate the move
                 stack[stack.length - 1].id = 'carouselRight'; //lable the imgs position
-                stack.pop();  //remove the img from the stack
+                stack.pop(); //remove the img from the stack
             }
 
             cs.id = 'carouselLeft'; //lable the imgs position
             cs.className = 'scaleOutToLeft'; //animate the move
 
-            break;
+            return;
     }
 }
 
 function carousel(side) {
     switch (side) {
         case 'left':
-            carouselScroll(document.getElementById('carouselLeft'));
-            break;
+            return carouselScroll(document.getElementById('carouselLeft'));
 
         case 'right':
-            carouselScroll(document.getElementById('carouselRight'));
-            break;
+            return carouselScroll(document.getElementById('carouselRight'));
     }
 }
-
-let hamburgerBtn = document.getElementById('hamburgerMenuBtn');
-hamburgerBtn.addEventListener('click', () => {
-    let hamburgerContent = document.getElementById('hamburgerContent');
-    switch(hamburgerBtn.checked) {
-        case true:
-            hamburgerContent.className = 'hamburgerContentVisible';
-            document.documentElement.className = 'htmlStopScroll';
-            break;
-
-        case false:
-            hamburgerContent.className = 'hamburgerContentInvisible';
-            document.documentElement.className = '';
-            break;
-    }
-});
